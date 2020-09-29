98f33aad89b3bab3a7d1aca1bd3dcec85f9c7d89
Jeremy Gann <56943728+Jgann123@users.noreply.github.com>
2020-09-29 17:47:34 -0400

    first commit

---
+++ src/main/java/com/graingerslanding/com/GraingersLanding1Application.java
@@ -0,0 +1,13 @@
+package com.graingerslanding.com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraingersLanding1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GraingersLanding1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/graingerslanding/com/GraingersLanding1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.graingerslanding.com;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraingersLanding1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
